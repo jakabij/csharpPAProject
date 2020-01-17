d83a5b4b4aa4e38867f1cf3891dcb69807c237a7
jakabij <jakabistvanj@gmail.com>
2020-01-17 15:11:05 +0100

    Added the VideoGame class which is a parent class

---
+++ PAProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PAProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ PAProject/VideoGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PAProject
+{
+    abstract class VideoGame
+    {
+        string Developer { get; set; }
+        string Name { get; set; }
+        int Rating { get; set; }
+        string Type { get; set; }
+
+        public abstract void EffectToPlayer();
+    }
+}
